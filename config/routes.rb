995b2822e7578a4401fcecd79ef7de2b84fb58a2
Mason Jarr <rrajnosam@gmail.com>
2020-04-24 18:11:15 -0700

    routes optimizer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+  devise_for :users
   resources :friendships, only: [:create, :destroy]
   resources :user_stocks, only: [:create, :destroy]
-  devise_for :users
+
     root to: 'welcome#index'
     get 'my_portfolio', to: 'users#my_portfolio'
     get 'search_stock', to: 'stocks#search'
